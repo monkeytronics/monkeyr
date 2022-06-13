b14fb85213d9b02186e71c263cab41ef6cdba3fd
Al Bren <al@monkeytronics.co.nz>
2022-06-13 21:41:26 +1200

    added make_nz_datetime function - all timezones

--- R/wrangle.R
+++ R/wrangle.R
@@ -1,18 +1,31 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Unix timestamp to local timezone datetime
+#' @description Convert a time stamp into local timezone datetime
+#' @param timestamp Unix time stamp vector
+#' @param long Longitude! as vector
+#' @param lat   Latitude! as vector
+#' @return Returns "POSIXct", "POSIXt" vector in NZ time zone
+#' @export
+#' @examples
+#' make_nz_datetime(1555555555, -125.357, 23.556)
+make_nz_datetime <- function(timestamp, long = 0, lat = 0) {
+  checkmate::assert_numeric(timestamp)
+  checkmate::assert_numeric(long)
+  checkmate::assert_numeric(lat)
 
-wrangle <- function() {
-  print("Hello, world!")
+  # test only - NZ Values
+  # https://stackoverflow.com/questions/59833660/
+  # convert-to-local-time-zone-from-latitude-and-longitude-r
+  # --------------------------------------------------------
+  # timestamp = 1655112631
+  # lat = -40.9006
+  # long = 174.8860
+
+  # Get local timezone baesd on lat & long
+  local_tz = tz_lookup_coords(lat = lat, lon = long, method = "accurate")
+
+  # convert to local time plus timezone format
+  local_date <- timestamp %>%
+    as.POSIXct(origin = "1970-01-01") %>%
+    lubridate::with_tz(tz = local_tz)
 }
+
