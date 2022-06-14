916d118ae57eb7fe1eade6b840858c50be375d1f
Al Bren <al@monkeytronics.co.nz>
2022-06-14 23:58:21 +1200

    more rough changes

--- R/0b-test-params.R
+++ R/0b-test-params.R
@@ -1,7 +1,7 @@
 #' test_params
-#' @description Access to raw test data. Data-sets include obs; devices;
-#' weather & intreventions, all as csv. Plus report params as txt file.
-#' We will collect datasets from real world tests as we go.
+#' @description Test Data! Data-sets include obs.csv; devices.csv;
+#' weather.csv & intreventions.csv. Plus report params as txt file.
+#' We will collect data-sets from real world tests as we go.
 #' Example data-sets might be : \cr
 #'   * All devices in data-set have no data.
 #'   * Only 1-2 devices in data-set. Mimic home user.
@@ -14,18 +14,18 @@ test_params <- function(data_folder = "monkey_a", params_file = "params_1.txt") 
   list(
 
     ## May only work in dev
-    obs           = paste0("inst/extdata/",   data_folder, "/obs.csv"),
-    weather       = paste0("inst/extdata/",   data_folder, "/weather.csv"),
-    dev           = paste0("inst/extdata/",   data_folder, "/dev.csv"),
-    interv        = paste0("inst/extdata/",   data_folder, "/interv.csv"),
-    params        = paste0("inst/extparams/", params_file),
+    # obs           = paste0("inst/test-data/",   data_folder, "/obs.csv"),
+    # weather       = paste0("inst/test-data/",   data_folder, "/weather.csv"),
+    # dev           = paste0("inst/test-data/",   data_folder, "/dev.csv"),
+    # interv        = paste0("inst/test-data/",   data_folder, "/interv.csv"),
+    # params        = paste0("inst/test-params/", params_file),
 
-    ## Using sysdata - production - USE THIS CODE?
-    # obs           = system.file("extdata", paste0(data_folder, "/obs.csv"),     package = "monkeyr"),
-    # weather       = system.file("extdata", paste0(data_folder, "/weather.csv"), package = "monkeyr"),
-    # dev           = system.file("extdata", paste0(data_folder, "/dev.csv"),     package = "monkeyr"),
-    # interv        = system.file("extdata", paste0(data_folder, "/interv.csv"),  package = "monkeyr"),
-    # params        = system.file("extparams", params_file),                      package = "monkeyr"),
+    ## Using sysdata - production - USE THIS CODE!
+    obs           = system.file("test-data", paste0(data_folder, "/obs.csv"),     package = "monkeyr"),
+    weather       = system.file("test-data", paste0(data_folder, "/weather.csv"), package = "monkeyr"),
+    dev           = system.file("test-data", paste0(data_folder, "/dev.csv"),     package = "monkeyr"),
+    interv        = system.file("test-data", paste0(data_folder, "/interv.csv"),  package = "monkeyr"),
+    params        = system.file("test-params", params_file,                       package = "monkeyr"),
 
 
     # replace these with text files with standard options.
